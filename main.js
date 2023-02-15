11129784b885db497fa3d14c5085374b38471262
Amanuel Haile <amanuelhaile369@gmail.com>
2023-02-15 14:20:59 +0300

    Create game Function that playes for five round
      - it also keeps score and reports a winner or loser at the end

--- main.js
+++ main.js
@@ -19,3 +19,45 @@ function playRound(playerSelection, computerSelection) {
     }
     return result;
 }
+
+function game() {
+    let playerCount = 0;
+    let computerCount = 0;
+
+    for(let i = 0; i < 5; i++) {
+        let playerSelection = "";
+        do{
+            playerSelection = prompt("Enter Your Choice :").toUpperCase();   
+            if(!choice.includes(playerSelection.toLowerCase())){
+                
+                alert("You can only Choice: " + choice.join(", ").toUpperCase()); 
+            }else{
+                break;
+            }
+        }while(!choice.includes(playerSelection.toLowerCase()));
+        const computerSelection = getComputerChoice().toUpperCase();
+
+        const result = playRound(playerSelection, computerSelection);
+
+        if(result == "Computer") {
+            console.log(`Round ${i+1}: You Lose! ${computerSelection} beats ${playerSelection}`)
+            computerCount++;
+        } else if (result == "Player") {
+            console.log(`Round ${i+1}: You Win! ${playerSelection} beats ${computerSelection}`)
+            playerCount++;
+        } else {
+            console.log(`Round ${i+1}: It's a Tie! ${playerSelection} ties with ${computerSelection}`)
+        }
+    }
+
+    if(playerCount > computerCount) {
+        console.log("You Win!");
+        alert("You Win!");
+    } else if(computerCount > playerCount) {
+        console.log("Your Loos!");
+        alert("Your Loos!");
+    } else {
+        console.log("It's a Tie!");
+        alert("It's a Tie!");
+    }
+}
