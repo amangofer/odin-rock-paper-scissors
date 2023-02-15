9830e032dc758c05e24ed0808abbcc1aa91acff5
Amanuel Haile <amanuelhaile369@gmail.com>
2023-02-15 14:19:22 +0300

    create playRound function that playes single round

--- main.js
+++ main.js
@@ -5,3 +5,17 @@ function getComputerChoice() {
     const random = Math.floor(Math.random() * choice.length)
     return choice[random];
 }
+
+function playRound(playerSelection, computerSelection) {
+    let result = "";
+    if(playerSelection == computerSelection) {
+        result = "tie";
+    } else if((playerSelection == "ROCK" && computerSelection == "SCISSORS") ||
+    (playerSelection == "PAPER" && computerSelection == "ROCK") ||
+    (playerSelection == "SCISSORS" && computerSelection == "PAPER")) {
+        result = "Player";
+    } else {
+        result = "Computer";
+    }
+    return result;
+}
